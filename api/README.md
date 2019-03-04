1c6652fc6c6498fdf2b9e5f96a1f7a7a39619924
Lidii <lidiiany92@gmail.com>
2019-03-04 18:43:30 -0500

    Reshufled files in folders

---
+++ api/README.md
@@ -0,0 +1,2 @@
+# File_sharing_web_app
+Creating file sharing web app with Node js, React from scratch.
---
+++ api/src/database.js
@@ -0,0 +1,11 @@
+import {MongoClient} from 'mongodb';
+
+const url = 'mongodb://localhost:27017/fileapp';
+
+export const connect = (cb) => {
+    MongoClient.connect(url, (err, db) => {
+        return cb(err, db);
+    });
+}
+
+
